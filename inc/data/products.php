1ca79d8eb570ec68a27cee96711649d8e5dc4eb5
Jeremy Castaing <104232891+Garcon2Jeu@users.noreply.github.com>
2023-04-17 11:23:32 +0200

    Initialize develop

---
+++ inc/data/products.php
@@ -0,0 +1,20 @@
+<?php
+
+$catalog = [
+    46 => [
+        'name' => 'Full chocolate cake',
+        'description' => 'Cooked by Penny !',
+    ],
+    36 => [
+        'name' => 'Macaroon and cream cake',
+        'description' => 'Cooked by Bernadette !',
+    ],
+    58 => [
+        'name' => 'Vanilla and chocolate cake',
+        'description' => 'Cooked by Bernadette !',
+    ],
+    32 => [
+        'name' => 'Vanilla and fruits cake',
+        'description' => 'Cooked by Penny !',
+    ],
+];
